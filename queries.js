1fc9b636eec17a737d75723608ab789f21f7a225
OsasuE <ehirhieme.osasu@gmail.com>
2022-02-26 12:44:41 -0800

    Luotu Tietokanta ja node.js pohja

---
+++ queries.js
@@ -0,0 +1,74 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'api',
+  password: '',
+  port: 5432,
+})
+
+const getDevices = (request, response) => {
+    pool.query('SELECT * FROM devices ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const getDeviceById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM devices WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const createDevice = (request, response) => {
+    const { name, serialNum } = request.body
+  
+    pool.query('INSERT INTO devices (name, serialNum) VALUES ($1, $2)', [name, serialNum], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Device added with ID: ${result.insertId}`)
+    })
+  }
+
+  const updateDevice = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, serialNum } = request.body
+  
+    pool.query(
+      'UPDATE devices SET name = $1, serialNum = $2 WHERE id = $3',
+      [name, serialNum, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Device modified with ID: ${id}`)
+      }
+    )
+  }
+
+  const deleteDevice= (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM devices WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Device deleted with ID: ${id}`)
+    })
+  }
+
+  module.exports = {
+    getDevices,
+    getDeviceById,
+    createDevice,
+    updateDevice,
+    deleteDevice,
+  }
